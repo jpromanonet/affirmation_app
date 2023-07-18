0856e0f8600039b2b35626b743d2155068ab2f5c
jpromanonet <programming@jpromano.net>
2023-07-18 18:14:33 -0300

    Almost done

--- mad-mercury/src/components/AffirmationComponent.jsx
+++ mad-mercury/src/components/AffirmationComponent.jsx
@@ -1,5 +1,9 @@
+const data = await fetch('https://e51pzppdu1.execute-api.us-east-1.amazonaws.com/items').then((response) =>
+    response.json()
+);
+
 const Affirmation = () => {
-    return <h3>my affirmation</h3>;
+    return <h3>{JSON.stringify(data[0].Text)}</h3>;
   };
         
   export default Affirmation;
